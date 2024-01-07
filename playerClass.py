36f2573416ae003ee95f82c6e8f4676f15b9463b
FabianKirner <26159742-FabianKirner@users.noreply.replit.com>
2024-01-07 17:26:07 +0000

    player json functions added

--- playerClass.py
+++ playerClass.py
@@ -1,19 +1,91 @@
+import json
+
+
 class Player:
 
   def __init__(self, username, password, location):
-    self.matchesWon = 0
-    self.matchesPlayed = 0
-    self.moneySpent = 0  # default currency in sterling pounds (converter needed)
-    self.moneyWon = 0
-    self.position = 0  # 1 is top
-    self.fixtures = []
-    self.username = username
-    self.password = password
-    self.location = location
+    self.__matchesWon = 0
+    self.__matchesPlayed = 0
+    self.__moneySpent = 0  # default currency in sterling pounds (converter needed)
+    self.__moneyWon = 0
+    self.__position = 0  # 1 is top
+    self.__fixtures = []
+    self.__username = username
+    self.__password = password
+    self.__location = location
 
   def showDetails(self):
     pass
 
+  #-----------JSON-----------
+
+  # def getUserData(self, username):
+  #   try:
+  #     with open("players.json", mode="r") as file:
+  #       userData = json.load(file)[username]
+  #       return userData
+  #   except json.JSONDecodeError:
+  #     return -1  # Error parsing from json
+  #   except FileNotFoundError:
+  #     return -2  # File not found
+
+  def getAllData(self):
+    try:
+      with open("players.json", mode="r") as file:
+        #allData = {"fabs": None}
+        allData = json.load(file)
+        return allData
+    except json.JSONDecodeError:
+      return -1  # Error parsing from json
+    except FileNotFoundError:
+      return -2  # File not found
+
+  def savePlayerData(self, username, freshUserData):
+    try:
+      allData = self.getAllData()
+      if type(allData) is dict:
+        if username in allData:
+          allData[username] = freshUserData
+          with open("players.json", mode="w") as file:
+            json.dump(allData, file, indent=4)
+        else:
+          return -3  # User not recorded
+    except json.JSONDecodeError:
+      return -1  # Error parsing from json
+    except FileNotFoundError:
+      return -2  # File not found
+
+  def addPlayer(self, username):
+    try:
+      allData = self.getAllData()
+      if type(allData) is dict:
+        # only initialises a pair "username": None, use savePlayerData() as well
+        if username not in allData:
+          allData[username] = None
+          with open("players.json", mode="w") as file:
+            json.dump(allData, file, indent=4)
+        else:
+          return -3  # User is already recorded, no changes made
+    except json.JSONDecodeError:
+      return -1  # Error parsing from json
+    except FileNotFoundError:
+      return -2  # File not found
+
+  def removePlayer(self, username):
+    try:
+      allData = self.getAllData()
+      if type(allData) is dict:
+        allData.pop(username)
+        with open("players.json", mode="w") as file:
+          json.dump(allData, file, indent=4)
+    except json.JSONDecodeError:
+      return -1  # Error parsing from json
+    except FileNotFoundError:
+      return -2  # File not found
+    except KeyError:
+      return -3  # User not recorded, this is fine
+
+  #-----------Player Functions-----------
   def registerPlayer(self):
     cancel = False
     while not cancel:
@@ -49,6 +121,11 @@ class Player:
   def setLocation(self, location):
     self.location = location
 
+  #===========Getters===========
+
+  def getUsername(self):
+    return self.__username
+
   #===========Methods===========
 
   def createUsername(self, usernames):  # check username is unique
@@ -75,3 +152,12 @@ class Player:
       if location not in locations:
         print("Invalid location.")
     return location
+
+
+testdict = {"location": "UK"}
+
+Fab = Player("fabi", "123", "UK")
+#print(Fab.getAllData())
+#print(Fab.removePlayer("misterman"))
+print(Fab.addPlayer("fab"))
+print(Fab.savePlayerData("fab", testdict))
